9f7fdc1ac07892ac47ae72c1aa988a21112f9a98
tylermorganme <tyler@tylermorgan.me>
2020-03-29 02:03:00 -0700

    Add test coverage

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Cell } from 'recharts'
 import _ from 'lodash'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Table } from 'react-bootstrap';
 import { useMainContext } from './providers/MainProvider'
+import statePopulationData from './utils/statePopulationData'
 
 const GET_STATES = gql`
   query luke {
@@ -115,7 +116,7 @@ const TestChart = ({ data }) => {
 const RateChart = ({ data }) => {
   const { setActiveState } = useMainContext()
   const { activeState } = useMainContext()
-  
+
   const onClick = (data) => {
     setActiveState(data.state)
   }
@@ -160,20 +161,70 @@ const RateChart = ({ data }) => {
 
 }
 
+const TestCoverageChart = ({ data }) => {
+  const { setActiveState } = useMainContext()
+  const { activeState } = useMainContext()
+
+  const onClick = (data) => {
+    setActiveState(data.state)
+  }
+
+  return (
+    <ResponsiveContainer width="100%" height={600}>
+      <BarChart data={data}
+        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="state" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar onClick={onClick} dataKey="testCoverage" stackId="a" fill={constants.colors.fill1}>
+          {data.map((entry, index) => (
+            <Cell
+              key={entry.state}
+              fill={
+                entry.state === activeState ?
+                  constants.colors.highlight1 :
+                  constants.colors.fill1
+              }
+            />
+          ))}
+        </Bar>
+      </BarChart>
+    </ResponsiveContainer>
+  );
+
+}
+
 const App = () => {
   const { loading, error, data } = useQuery(GET_STATES);
-  const [stateData, setStateData] = useState({})
+  const [stateData, setStateData] = useState([])
+
+  console.log(statePopulationData)
 
   useEffect(() => {
     if (data) {
-      let modifiedData = data['state'].map((item) => {
-        return {
-          ...item,
-          positiveRate: item['positive'] / item['totalTestResults'],
-          negativeRate: 1 - item['positive'] / item['totalTestResults'],
-        }
-      });
-      setStateData(modifiedData.filter(item => !territories.includes(item['state'])))
+      let modifiedData = data['state']
+        .filter(item => !territories.includes(item['state']))
+        .map((state) => {
+          // console.log(state)
+          const positiveRate = state['positive'] / state['totalTestResults']
+          const negativeRate = 1 - state['positive'] / state['totalTestResults']
+          const population = statePopulationData[state['state']]['population']
+          const density = statePopulationData[state['state']]['density']
+          const name = statePopulationData[state['state']]['name']
+          return {
+            ...state,
+            positiveRate,
+            negativeRate,
+            population,
+            density,
+            name,
+            testCoverage: state['totalTestResults']/population,
+            deathPerMillion: state['death']/(population/1000000)
+          }
+        });
+      setStateData(modifiedData)
     }
   }, [data])
 
@@ -195,12 +246,65 @@ const App = () => {
           <RateChart
             data={_.sortBy(stateData, 'positiveRate')}
           />
-          {JSON.stringify(stateData)}
         </Col>
       </Row>
       <Row>
         <Col>
-          {JSON.stringify(stateData)}
+          <TestCoverageChart
+            data={_.sortBy(stateData, 'testCoverage')}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Table striped bordered responsive>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>State</th>
+                <th>Positive</th>
+                <th>Negative</th>
+                <th>Negative Score</th>
+                <th>Negative Regular Score</th>
+                <th>Commercial Score</th>
+                <th>Score</th>
+                <th>Grade</th>
+                <th>Total Test Results</th>
+                <th>Hospitalized</th>
+                <th>Death</th>
+                <th>Population</th>
+                <th>Density</th>
+                <th>Date Modified</th>
+                <th>Date Checked</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                stateData.map((state) => {
+                  return (
+                    <tr key={state['state']}>
+                      <td>{state['name']}</td>
+                      <td>{state['state']}</td>
+                      <td>{state['positive']}</td>
+                      <td>{state['negative']}</td>
+                      <td>{state['negativeScore']}</td>
+                      <td>{state['negativeRegularScore']}</td>
+                      <td>{state['commercialScore']}</td>
+                      <td>{state['score']}</td>
+                      <td>{state['grade']}</td>
+                      <td>{state['totalTestResults']}</td>
+                      <td>{state['hospitalized']}</td>
+                      <td>{state['death']}</td>
+                      <td>{state['population']}</td>
+                      <td>{state['density']}</td>
+                      <td>{state['dateModified']}</td>
+                      <td>{state['dateChecked']}</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </Table>
         </Col>
       </Row>
     </Container>
