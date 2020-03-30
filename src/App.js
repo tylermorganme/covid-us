132be79008056c88cf9c57ce42be50686568755e
tylermorganme <tyler@tylermorgan.me>
2020-03-29 21:52:01 -0700

    Add consistent aspect ratio for graphs

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const StackedBarChart = ({ data, seriesList, xTickFormatter, sortBy, title }) =>
   const chartData = _.sortBy(data, sortBy)
 
   return (
-    <ResponsiveContainer width="100%" height={600}>
+    <ResponsiveContainer width="100%" height={Math.min(windowWidth/2, 600)}>
       <BarChart
         data={chartData}
         // margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
