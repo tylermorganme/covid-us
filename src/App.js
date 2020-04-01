6c30252919c25c6f886f434c3c69d004a1607208
tylermorganme <tyler@tylermorgan.me>
2020-03-31 18:57:12 -0700

    Remove url meta tags

--- src/App.js
+++ src/App.js
@@ -391,7 +391,7 @@ const App = () => {
             <Container >
               <p className="lead"><strong>Welcome to COVID Stats U.S.</strong></p>
               <p className="lead"> This project started out as my own attempt to understand what the COVID situation looked like in the U.S. and rapidly grew into a tool that I hope you will find useful. This is a work in progress so stay-tuned for updates.</p>
-              <p className="lead">The majority of the data is provided by the hard work of the folks at <a href="http://www.covidtracking.com/">covidtracking.com</a>.</p>
+              <p className="lead">The charts represent the latest data available thanks to the hard work of the folks at <a href="http://www.covidtracking.com/">covidtracking.com</a>. If anything looks off, make sure to check their <a href="https://covidtracking.com/data/"> notes for that state</a>.</p>
               <p className="lead">Clicking on the data for any state will highlight that state in all graphs.</p>
               <p className="lead">If you have questions or comments <a href="https://twitter.com/TylerMorganMe"> feel free to reach out</a>.</p>
               <p className="lead">Enjoy and please share!</p>
